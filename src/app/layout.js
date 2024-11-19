128851e0779a8b5540fd83b60c1b3d2aaf36629c
MUDIT ANAND <102852265+Webolove@users.noreply.github.com>
2024-11-19 12:02:03 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Toaster } from 'react-hot-toast'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
